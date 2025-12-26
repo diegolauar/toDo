c2f0c1925bdacb16f5bee1c202db6ecf22159074
diegolauar <diego_pktd@hotmail.com>
2025-12-26 16:31:40 -0300

    project toDo

---
+++ src/vite.env.d.ts
@@ -0,0 +1,10 @@
+// src/env.d.ts
+
+/// <reference types="vite/client" />
+/// <reference types="vite-plugin-svgr/client" />
+
+declare module '*.svg?react' {
+  import { FC, SVGProps } from 'react'
+  const ReactComponent: FC<SVGProps<SVGSVGElement>>
+  export default ReactComponent
+}
